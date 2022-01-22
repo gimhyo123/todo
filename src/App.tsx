d013e2633cf30a0aaeee3d1414f033cb3933d17f
gimyo <gimh81040@gmail.com>
2022-01-22 10:37:50 +0900

    프로젝트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React, { useDebugValue, useState } from 'react';
-import './App.css';
-import Todo from './Todo';
+import React from 'react';
 
 function App() {
- 
   return (
     <div className="App">
-    <Todo/>
-
-
+      
     </div>
   );
 }
