3bff6e25f37d948d280981d879bcb66808528dc1
gimhyojun2 <21it20806@sonline20.sen.go.kr>
2022-01-22 09:49:44 +0900

    투두리스트 끝

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useDebugValue, useState } from 'react';
+import './App.css';
+import Todo from './Todo';
+
+function App() {
+ 
+  return (
+    <div className="App">
+    <Todo/>
+
+
+    </div>
+  );
+}
+
+export default App;
